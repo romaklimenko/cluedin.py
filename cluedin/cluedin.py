2fe4d4a2fc879c2e19cb3c9e1591aa482f9550e3
Roman Klimenko <rok@cluedin.com>
2022-02-14 23:15:53 +0100

    WIP: rules to querybuilder

--- cluedin/cluedin.py
+++ cluedin/cluedin.py
@@ -43,3 +43,25 @@ def search(token, api_url, query, entries):
 
   return response.json()
 
+def get_rule(token, api_url, rule_id):
+  headers = {
+    'Authorization': 'Bearer {}'.format(token),
+    'Content-Type': 'application/json'
+  }
+
+  response = requests.get(
+    f'{api_url}/rules/{rule_id}',
+    headers=headers)
+
+  return response.json()
+
+def rule_json_to_querybuilder():
+  pass
+
+def rule_operator_id_to_querybuilder_string(rule_operator_id):
+  match rule_operator_id:
+    case '0bafc522-8011-43ba-978a-babe222ba466':
+      return 'equal'
+    # TODO: add more cases
+    case _:
+      return None
